7ecc615f9983c03b0679c25d0449507df228d64a
H0xC0000005 <74247664+H0xC0000005@users.noreply.github.com>
2023-10-26 11:08:06 +0800

    try to push to 3090

---
+++ test.py
@@ -0,0 +1,40 @@
+import osmnx as ox
+import time
+from shapely.geometry import Polygon
+import os
+
+def save_graph_shapefile_directional(G, filepath=None, encoding="utf-8"):
+    # default filepath if none was provided
+    if filepath is None:
+        filepath = os.path.join(ox.settings.data_folder, "graph_shapefile")
+
+    # if save folder does not already exist, create it (shapefiles
+    # get saved as set of files)
+    if not filepath == "" and not os.path.exists(filepath):
+        os.makedirs(filepath)
+    filepath_nodes = os.path.join(filepath, "nodes.shp")
+    filepath_edges = os.path.join(filepath, "edges.shp")
+
+    # convert undirected graph to gdfs and stringify non-numeric columns
+    gdf_nodes, gdf_edges = ox.utils_graph.graph_to_gdfs(G)
+    gdf_nodes = ox.io._stringify_nonnumeric_cols(gdf_nodes)
+    gdf_edges = ox.io._stringify_nonnumeric_cols(gdf_edges)
+    # We need an unique ID for each edge
+    gdf_edges["fid"] = gdf_edges.index
+    # save the nodes and edges as separate ESRI shapefiles
+    gdf_nodes.to_file(filepath_nodes, encoding=encoding)
+    gdf_edges.to_file(filepath_edges, encoding=encoding)
+
+print("osmnx version",ox.__version__)
+
+# format is (long, long, lat, lat). E and N are positive, W and S are negative
+bounds = (18.029122582902115, 18.070836297501724, 59.33476653724975, 59.352622230576124)
+x1,x2,y1,y2 = bounds
+boundary_polygon = Polygon([(x1,y1),(x2,y1),(x2,y2),(x1,y2)])
+G = ox.graph_from_polygon(boundary_polygon, network_type='drive')
+start_time = time.time()
+save_graph_shapefile_directional(G, filepath='./stockholm')
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
