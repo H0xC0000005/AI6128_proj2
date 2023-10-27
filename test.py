7684976c7f11b683a7587da3c24b5f37b466f192
H0xC0000005 <zh0001hu@e.ntu.edu.sg>
2023-10-27 18:44:47 +0800

    finished crawl part v1

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import osmnx as ox
 import time
 from shapely.geometry import Polygon
 import os
+import pandas as pd
+
 
 def save_graph_shapefile_directional(G, filepath=None, encoding="utf-8"):
     # default filepath if none was provided
@@ -20,21 +22,28 @@ def save_graph_shapefile_directional(G, filepath=None, encoding="utf-8"):
     gdf_nodes = ox.io._stringify_nonnumeric_cols(gdf_nodes)
     gdf_edges = ox.io._stringify_nonnumeric_cols(gdf_edges)
     # We need an unique ID for each edge
+    # NOTE: tuple format not supported. cannot directly use gdf_edges.index since
+    # this is a tuple returned. must post process.
     gdf_edges["fid"] = gdf_edges.index
+    gdf_edges["fid"] = gdf_edges["fid"].apply(lambda x: str(x))
     # save the nodes and edges as separate ESRI shapefiles
+    # NOTE: may raise ValueError for invalid entry type, e.g. tuple, list, bytes
     gdf_nodes.to_file(filepath_nodes, encoding=encoding)
     gdf_edges.to_file(filepath_edges, encoding=encoding)
-
-print("osmnx version",ox.__version__)
-
-# format is (long, long, lat, lat). E and N are positive, W and S are negative
-bounds = (18.029122582902115, 18.070836297501724, 59.33476653724975, 59.352622230576124)
-x1,x2,y1,y2 = bounds
-boundary_polygon = Polygon([(x1,y1),(x2,y1),(x2,y2),(x1,y2)])
-G = ox.graph_from_polygon(boundary_polygon, network_type='drive')
-start_time = time.time()
-save_graph_shapefile_directional(G, filepath='./stockholm')
-print("--- %s seconds ---" % (time.time() - start_time))
+    pass
 
 
+print("osmnx version", ox.__version__)
 
+# # format is (long, long, lat, lat). E and N are positive, W and S are negative
+# bounds = (18.029122582902115, 18.070836297501724, 59.33476653724975, 59.352622230576124)
+# x1, x2, y1, y2 = bounds
+# boundary_polygon = Polygon([(x1, y1), (x2, y1), (x2, y2), (x1, y2)])
+# G = ox.graph_from_polygon(boundary_polygon, network_type="drive")
+# start_time = time.time()
+# save_graph_shapefile_directional(G, filepath="./stockholm")
+# print("--- %s seconds ---" % (time.time() - start_time))
+
+place = "Stockholm, Sweden"
+G = ox.graph_from_place(place, network_type="drive", which_result=1)
+save_graph_shapefile_directional(G, filepath="stockholm")
